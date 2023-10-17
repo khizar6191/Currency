5c7e1b3b3814f2de9a027a63d953c4b27f3d9a65
unknown <khizarbagwan786@gmail.com>
2023-10-17 19:31:15 +0530

    new code

--- WebApi/Controllers/CurrencyController.cs
+++ WebApi/Controllers/CurrencyController.cs
@@ -5,7 +5,6 @@ using Newtonsoft.Json;
 using WebApi.Models;
 using WebApi.Services;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApi.Controllers
 {
@@ -69,53 +68,6 @@ namespace WebApi.Controllers
             return Ok(data);
         }
 
-
-        //implemented ilogger and filebased logging
-        //public async Task<IActionResult> Get(string From,String To,double Amount )
-        //{
-
-        //    using (FileStream fs = new FileStream("C:\\Users\\User\\source\\repos\\Currency\\WebApi\\log.txt", FileMode.Append, FileAccess.Write))
-        //    {
-        //       
-
-        //        using (var _client = new HttpClient())
-        //        {
-        //            _client.BaseAddress = new Uri("https://v6.exchangerate-api.com/");
-        //            using (HttpResponseMessage response = await _client.GetAsync($"v6/bafa7500d6a917cc848033e3/latest/{From}"))
-        //            {
-        //                string cacheEntry="";
-        //                
-        //                _log.Ilog("the type of request send " + response.RequestMessage.Method + " on api " + response.RequestMessage.RequestUri);
-        //                if (response.IsSuccessStatusCode)
-        //                {
-        //                    var json = await response.Content.ReadAsStringAsync();
-        //                    var data = JsonConvert.DeserializeObject<Currecny>(json);
-        //                    double amt = 0;
-
-        //                    if (data.Conversion_rates.ContainsKey(To))
-        //                    {
-        //                        double exchangeRate = data.Conversion_rates[To];
-        //                        amt = exchangeRate * Amount;
-        //                    }
-
-
-        //                    data.amt = Math.Round(amt, 2);
-        //                    _log.Ilog("the recive response " + From + " of amount " + Amount + " to " + To + " " + data.amt);
-        //                    sw.WriteLine("the recive response " + From + " of amount " + Amount + " to " + To + " " + data.amt+ "\n");
-        //                    sw.Close();
-        //                    fs.Close();
-        //                    data.Conversion_rates = null;
-        //                    return Ok(data);
-        //                }
-        //                else
-        //                {
-        //                    // Handle API error responses
-        //                    return BadRequest();
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
 
             }
 }
