c4fe8279fc69bddc694e929d7f324ce41cab3d1c
unknown <khizarbagwan786@gmail.com>
2023-10-17 19:03:32 +0530

    updated version

--- WebApi/Controllers/CurrencyController.cs
+++ WebApi/Controllers/CurrencyController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
 using WebApi.Models;
 using WebApi.Services;
@@ -11,95 +13,99 @@ namespace WebApi.Controllers
     [ApiController]
     public class CurrencyController : ControllerBase
     {
-        IRepo _log;
-        public CurrencyController(IRepo log) { 
+       private readonly IRepo _log;
+        private IMemoryCache _cache;
+
+        public CurrencyController(IRepo log,IMemoryCache Imem) {
+        _cache = Imem;
         _log = log;
         }
+
         [HttpGet]
-        public async Task<IActionResult> Get(string From,String To,double Amount )
+        public async Task<IActionResult> Get(string From, string To, double Amount)
         {
-            using (var _client = new HttpClient())
+            if (!_cache.TryGetValue("currencies", out Currecny data))
             {
-                _client.BaseAddress = new Uri("https://v6.exchangerate-api.com/");
-                using (HttpResponseMessage response = await _client.GetAsync($"v6/bafa7500d6a917cc848033e3/latest/{From}"))
+                using (var _client = new HttpClient())
                 {
-                    _log.Ilog("the type of request send "+response.RequestMessage.Method+" on api "+response.RequestMessage.RequestUri);
-                    if (response.IsSuccessStatusCode)
+                    _client.BaseAddress = new Uri("https://v6.exchangerate-api.com/");
+                    using (HttpResponseMessage response = await _client.GetAsync($"v6/bafa7500d6a917cc848033e3/latest/{From}"))
                     {
-                        var json = await response.Content.ReadAsStringAsync();
-                        var data = JsonConvert.DeserializeObject<Currecny>(json);
-                        double amt=0;
-                       
-                        if (data.Conversion_rates.ContainsKey(To))
+                        if (response.IsSuccessStatusCode)
                         {
-                            double exchangeRate = data.Conversion_rates[To];    
-                            amt = exchangeRate*Amount;
+                            var json = await response.Content.ReadAsStringAsync();
+                            data = JsonConvert.DeserializeObject<Currecny>(json);//deserializing the dataaaa
+
+                           
+                            _cache.Set("currencies", data,  TimeSpan.FromMinutes(10));
                         }
-                        
-                       
-                        data.amt = Math.Round(amt,2);
-                        _log.Ilog("the recive response "+From + " of amount " + Amount + " to "+To+" "+ data.amt);
-                        data.Conversion_rates = null;
-                        return Ok(data);
-                    }
-                    else
-                    {
-                        // Handle API error responses
-                        return BadRequest();
+                        else
+                        {
+                           
+                            return BadRequest();
+                        }
                     }
                 }
             }
+
+            double amt = 0;
+            if (data.Conversion_rates.ContainsKey(To))
+            {
+                double exchangeRate = data.Conversion_rates[To];
+                amt = exchangeRate * Amount;
+            }
+
+            data.amt = Math.Round(amt, 2);
+            return Ok(data);
         }
 
-        //public IActionResult GetApi()
+
+        //implemented ilogger and filebased logging
+        //public async Task<IActionResult> Get(string From,String To,double Amount )
         //{
-        //    using (var _client = new HttpClient())
+
+        //    using (FileStream fs = new FileStream("C:\\Users\\User\\source\\repos\\Currency\\WebApi\\log.txt", FileMode.Append, FileAccess.Write))
         //    {
-        //        _client.BaseAddress = new Uri("http://data.fixer.io/");
-        //        using (HttpResponseMessage response = _client.GetAsync("api/latest?access_key=b691f5f8b1416727f7598f582f8ea77b&symbols=USD,AUD,CAD,PLN,MXN").Result)
+        //        StreamWriter sw = new StreamWriter(fs);
+
+        //        using (var _client = new HttpClient())
         //        {
-        //            if (response.IsSuccessStatusCode)
+        //            _client.BaseAddress = new Uri("https://v6.exchangerate-api.com/");
+        //            using (HttpResponseMessage response = await _client.GetAsync($"v6/bafa7500d6a917cc848033e3/latest/{From}"))
         //            {
-        //                var json = response.Content.ReadAsStringAsync().Result;//here result is used to block the current thread to block and wait till the operation is complete
-        //                var data = JsonConvert.DeserializeObject<Currecny>(json);
-        //                return Ok(data);
-        //            }
-        //            else
-        //            {
-        //                // Handle API error responses
-        //                return StatusCode((int)response.StatusCode, "Failed to fetch data from the API.");
+        //                string cacheEntry="";
+        //                
+        //                _log.Ilog("the type of request send " + response.RequestMessage.Method + " on api " + response.RequestMessage.RequestUri);
+        //                if (response.IsSuccessStatusCode)
+        //                {
+        //                    var json = await response.Content.ReadAsStringAsync();
+        //                    var data = JsonConvert.DeserializeObject<Currecny>(json);
+        //                    double amt = 0;
+
+        //                    if (data.Conversion_rates.ContainsKey(To))
+        //                    {
+        //                        double exchangeRate = data.Conversion_rates[To];
+        //                        amt = exchangeRate * Amount;
+        //                    }
+
+
+        //                    data.amt = Math.Round(amt, 2);
+        //                    _log.Ilog("the recive response " + From + " of amount " + Amount + " to " + To + " " + data.amt);
+        //                    sw.WriteLine("the recive response " + From + " of amount " + Amount + " to " + To + " " + data.amt+ "\n");
+        //                    sw.Close();
+        //                    fs.Close();
+        //                    data.Conversion_rates = null;
+        //                    return Ok(data);
+        //                }
+        //                else
+        //                {
+        //                    // Handle API error responses
+        //                    return BadRequest();
+        //                }
         //            }
         //        }
         //    }
         //}
-        //working code
-        //public async Task<IActionResult> Get()
-        //{
-        //    using (var _client = new HttpClient())
-        //    {
-        //        _client.BaseAddress = new Uri("http://data.fixer.io/");
-        //        using (HttpResponseMessage response =await _client.GetAsync("api/latest?access_key=b691f5f8b1416727f7598f582f8ea77b&symbols=USD,AUD,CAD,PLN,MXN")) 
-        //        {
-        //            var res = response.Content.ReadAsStringAsync().Result;
-        //            response.EnsureSuccessStatusCode();
-        //            return Ok(res);
-        //        }
-        //    }
-        //}
 
-
-
-        //public List<Currecny> Get()
-        //{
-        //    List<Currecny> list = new List<Currecny>();
-        //    HttpResponseMessage response = _client.GetAsync(baseAddress).Result;
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        string data = response.Content.ReadAsStringAsync().Result;
-        //        list = JsonConvert.DeserializeObject<List<Currecny>>(data);
-        //    }
-        //    return list;
-        //}
-
-    }
+            }
 }
